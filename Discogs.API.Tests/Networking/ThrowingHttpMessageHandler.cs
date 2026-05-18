ab165aff2057947794c0aecdf20336dd635d9541
Giulio Porcu <giulioporcu+github@outlook.com>
2026-05-18 16:20:40 +0200

    Removed test project in favor, implement later

--- Discogs.API.Tests/Networking/ThrowingHttpMessageHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Discogs.API.Tests.Networking
-{
-    internal sealed class ThrowingHttpMessageHandler : HttpMessageHandler
-    {
-        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) => throw new HttpRequestException("Network error");
-    }
-}
